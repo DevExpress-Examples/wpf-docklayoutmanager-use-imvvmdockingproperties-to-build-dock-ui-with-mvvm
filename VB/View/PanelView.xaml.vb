5f6fcd9c186ae08726fd2ada89c1456a4e3bde06
DevExpressExampleBot <fakeEmail>
2022-07-07 13:20:18 +0400

    Source auto update [skip ci]

--- VB/View/PanelView.xaml.vb
+++ VB/View/PanelView.xaml.vb
@@ -1,26 +1,15 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace WpfApplication.View
-	''' <summary>
-	''' Interaction logic for PanelView.xaml
-	''' </summary>
-	Partial Public Class PanelView
-		Inherits UserControl
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+
+    ''' <summary>
+    ''' Interaction logic for PanelView.xaml
+    ''' </summary>
+    Public Partial Class PanelView
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
